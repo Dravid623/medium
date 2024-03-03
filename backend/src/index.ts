2340553ec9a4ae3fde7cb18103a4ac17cdf71456
dravid <dravidsunita623@gmail.com>
2024-03-03 17:00:08 +0530

    basic medium backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,18 @@
 import { Hono } from 'hono'
+import {userRouter} from './routes/user'
+import {blogRouter} from './routes/blog'
 
-const app = new Hono()
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string,
+    JWT_SECRET: string,
+  }
+}>()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.route('/api/v1/user',userRouter)
+app.route('/api/v1/blog',blogRouter)
 
 export default app
+
+// DATABASE_URL="postgresql://dravid1001:V6deJMa4ATXU@ep-empty-frog-a1v735gs-pooler.ap-southeast-1.aws.neon.tech/test?sslmode=require"
+// prisma://accelerate.prisma-data.net/?api_key=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlfa2V5IjoiZDlmMWJiODEtMTFmMS00MGZlLWI2NWItMzk5NjViMzg1NmExIiwidGVuYW50X2lkIjoiYWVlZTc1OWFlZDUxMzFlNjJhM2ZiZjE5NDZhZGIzMmIxZTNhNTQzM2Q4ZTFmY2MyNzZhN2UwODRjODQ2YmI0NSIsImludGVybmFsX3NlY3JldCI6ImU5MThjZmE0LWVlYjgtNGE0NC1iM2M4LTZiZDM5ZmU4NDU2NSJ9.PFYtbSQbkwMCfEX2x8IN8AJFxR5vvG0Mvx84VtrGzRk
---
+++ backend/src/routes/blog.ts
@@ -0,0 +1,106 @@
+import { Hono } from "hono";
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { decode, sign, verify } from 'hono/jwt'
+export const blogRouter = new Hono<{
+  Bindings: {
+    DATABASE_URL: string,
+    JWT_SECRET: string,
+  },
+  Variables: {
+    userId: string;
+  }
+}>()
+
+blogRouter.use("/*", async(c,next)=>{
+    const authHeader = c.req.header('authorization') || ""
+try {
+        const user = await verify(authHeader, c.env.JWT_SECRET)
+        if(user){
+            c.set("userId", user.id)
+            await next()
+        } else {
+            c.status(403);
+            return c.json({
+                message: "you are not logged in"
+            })
+        }
+} catch (error) {
+    c.status(403);
+    return c.json({
+        message: "you are not logged in"
+    })
+}
+})
+
+blogRouter.post('/', async(c) => {
+    const body = await c.req.json();
+    const authorId = c.get("userId")
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+}).$extends(withAccelerate())
+    const post = await prisma.post.create({
+    data: {
+        title: body.title,
+        content: body.content,
+        authorId: authorId
+    }
+})
+return c.json({
+    id: post.id
+})
+})
+
+blogRouter.put('/', async(c) => {
+   const body = await c.req.json();
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+}).$extends(withAccelerate())
+    const post = await prisma.post.update({
+        where: {
+            id: body.id
+        },
+    data: {
+        title: body.title,
+        content: body.content,
+    }
+})
+return c.json({
+    id: post.id
+})
+})
+
+// todo : add pagination here 
+blogRouter.get('/bulk', async(c) => {
+    const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+}).$extends(withAccelerate())
+const posts = await prisma.post.findMany();
+return c.json({
+    posts
+})
+})
+
+blogRouter.get('/:id', async(c) => {
+   const id = await c.req.param("id");
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+}).$extends(withAccelerate())
+    try {
+        const post = await prisma.post.findFirst({
+            where: {
+                id: id
+            }
+    })
+    return c.json({
+    post
+})
+    } catch (error) {
+        c.status(411)
+        return c.json({
+            message: "error while featching"
+        })
+    }
+
+})
+
