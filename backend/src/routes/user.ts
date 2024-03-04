58b6fcfa2deaa8c954620d7f98f66c53fe530f47
dravid <dravidsunita623@gmail.com>
2024-03-04 23:08:49 +0530

    @dravid/medium-common npm package published

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -2,7 +2,7 @@ import { Hono } from "hono";
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 import { decode, sign, verify } from 'hono/jwt'
-// import { signupInput } from "@dravid/medium-common"
+import { signupInput } from "@dravid/medium-common"
 export const userRouter = new Hono<{
   Bindings: {
     DATABASE_URL: string,
@@ -12,13 +12,13 @@ export const userRouter = new Hono<{
 
 userRouter.post('/signup', async(c) => {
   const body = await c.req.json();
-//   const { success } = signupInput.safeParse(body)
-// if(!success){
-//   c.status(411);
-//   return c.json({
-//     message: "input not correct"
-//   })
-// }
+  const { success } = signupInput.safeParse(body)
+if(!success){
+  c.status(411);
+  return c.json({
+    message: "input not correct"
+  })
+}
   const prisma = new PrismaClient({
     datasourceUrl: c.env.DATABASE_URL,
 }).$extends(withAccelerate())
